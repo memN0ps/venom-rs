174c531fc61fc1363b27176b4a753306c9d8dfb3
memN0ps <89628341+memN0ps@users.noreply.github.com>
2022-06-17 21:10:04 +1200

    Updater loader.rs
    * Added sanity check for e_lfanew
    * Commented out rva_to_file_offset as it is not required
    * Modified get_module_exports()

--- loader/src/loader.rs
+++ loader/src/loader.rs
@@ -1,6 +1,6 @@
-use std::{arch::asm, ffi::CStr, collections::BTreeMap};
+use std::{arch::asm, ffi::CStr, collections::BTreeMap, mem::size_of};
 
-use winapi::{um::winnt::{PIMAGE_DOS_HEADER, IMAGE_DOS_SIGNATURE, PIMAGE_NT_HEADERS, PIMAGE_SECTION_HEADER, PIMAGE_NT_HEADERS64, IMAGE_DIRECTORY_ENTRY_EXPORT, PIMAGE_EXPORT_DIRECTORY, IMAGE_NT_SIGNATURE}, ctypes::c_void};
+use winapi::{um::winnt::{PIMAGE_DOS_HEADER, IMAGE_DOS_SIGNATURE, PIMAGE_NT_HEADERS64, IMAGE_DIRECTORY_ENTRY_EXPORT, PIMAGE_EXPORT_DIRECTORY, IMAGE_DOS_HEADER}};
 use ntapi::{ntpebteb::PTEB, ntldr::{PLDR_DATA_TABLE_ENTRY}, ntpsapi::PEB_LDR_DATA};
 use wchar::wch;
 
@@ -35,8 +35,14 @@ pub fn reflective_loader() {
         dos_header = ret as PIMAGE_DOS_HEADER;
 
         if unsafe { (*dos_header).e_magic == IMAGE_DOS_SIGNATURE } {
-            println!("[+] IMAGE_DOS_HEADER: {:?}", dos_header);
-            break;
+			// some x64 dll's can trigger a bogus signature (IMAGE_DOS_SIGNATURE == 'POP r10'),
+			// we sanity check the e_lfanew with an upper threshold value of 1024 to avoid problems.
+            let header_value = unsafe { (*dos_header).e_lfanew };
+            
+            if header_value >= size_of::<IMAGE_DOS_HEADER>().try_into().unwrap() && header_value < 1024 {
+                println!("[+] IMAGE_DOS_HEADER: {:?}", dos_header);
+                break;
+            }
         }
         ret = ret - 1;
     }
@@ -65,17 +71,14 @@ pub fn reflective_loader() {
     println!("NTDLL: {:?}", ntdll_base);
 
     // get exports by hash
-    let loadliba_hash = get_exports_by_hash(kernel32_base as _, LOADLIBRARYA_HASH).expect("failed to get exports by hash");
+    let loadliba_hash = get_exports_by_hash(kernel32_base, LOADLIBRARYA_HASH).expect("failed to get exports by hash");
     println!("[+] LoadLibraryA {:?}", loadliba_hash);
 
-
     // STEP 2: load our image into a new permanent location in memory...
-
-    pause();
 }
 
 /// Gets exports by hash
-fn get_exports_by_hash(module_base: *mut c_void, hash: u32) -> Option<*mut u8> {
+fn get_exports_by_hash(module_base: *mut u8, hash: u32) -> Option<*mut u8> {
 
     // loop through the module exports to find export by hash
     for (name, addr) in unsafe { get_module_exports(module_base) } {
@@ -87,50 +90,51 @@ fn get_exports_by_hash(module_base: *mut c_void, hash: u32) -> Option<*mut u8> {
     return None;
 }
 
-/// gets the image export directory table
-unsafe fn get_module_exports(module_base: *mut c_void) -> BTreeMap<String, usize> {
-
+/// Retrieves all function and addresses from the specfied modules
+unsafe fn get_module_exports(module_base: *mut u8) -> BTreeMap<String, usize> {
     let mut exports = BTreeMap::new();
-
+    
     let dos_header = module_base as PIMAGE_DOS_HEADER;
 
-    #[cfg(target_arch = "x86_64")]
     let nt_header = (module_base as usize + (*dos_header).e_lfanew as usize) as PIMAGE_NT_HEADERS64;
-    #[cfg(target_arch = "x86")]
-    let nt_header = (module_base as usize + (*dos_header).e_lfanew as usize) as PIMAGE_NT_HEADERS;
 
-    let export_directory = rva_to_file_offset_pointer(module_base as usize, 
-        (*nt_header).OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT as usize].VirtualAddress as u32) as PIMAGE_EXPORT_DIRECTORY;
-    
+    let export_directory = (module_base as usize
+        + (*nt_header).OptionalHeader.DataDirectory
+            [IMAGE_DIRECTORY_ENTRY_EXPORT as usize]
+            .VirtualAddress as usize)
+        as PIMAGE_EXPORT_DIRECTORY;
+
     let names = core::slice::from_raw_parts(
-        rva_to_file_offset_pointer(module_base as usize, (*export_directory).AddressOfNames) as *const u32,
+        (module_base as usize + (*export_directory).AddressOfNames as usize)
+            as *const u32,
         (*export_directory).NumberOfNames as _,
     );
-    
+
     let functions = core::slice::from_raw_parts(
-        rva_to_file_offset_pointer(module_base as usize, (*export_directory).AddressOfFunctions) as *const u32,
+        (module_base as usize + (*export_directory).AddressOfFunctions as usize)
+            as *const u32,
         (*export_directory).NumberOfFunctions as _,
     );
-    
+
     let ordinals = core::slice::from_raw_parts(
-        rva_to_file_offset_pointer(module_base as usize, (*export_directory).AddressOfNameOrdinals) as *const u16,
+        (module_base as usize + (*export_directory).AddressOfNameOrdinals as usize)
+            as *const u16,
         (*export_directory).NumberOfNames as _,
     );
 
     println!("[+] Module Base: {:?} Export Directory: {:?} AddressOfNames: {names:p}, AddressOfFunctions: {functions:p}, AddressOfNameOrdinals: {ordinals:p} ", module_base, export_directory);
-    println!("Number of Names: {:?}", (*export_directory).NumberOfNames);
 
     for i in 0..(*export_directory).NumberOfNames {
 
-        let name = rva_to_file_offset_pointer(module_base as usize, names[i as usize]) as *const i8;
+        let name = (module_base as usize + names[i as usize] as usize) as *const i8;
 
         if let Ok(name) = CStr::from_ptr(name).to_str() {
-            
+
             let ordinal = ordinals[i as usize] as usize;
 
             exports.insert(
-                name.to_string(), 
-                rva_to_file_offset_pointer(module_base as usize, functions[ordinal])
+                name.to_string(),
+                module_base as usize + functions[ordinal] as usize,
             );
         }
     }  
@@ -164,6 +168,7 @@ pub fn get_loaded_module_by_hash(ldr: &PEB_LDR_DATA, hash: u32) -> Option<*mut u
 	None
 }
 
+/* 
 unsafe fn rva_to_file_offset_pointer(module_base: usize, mut rva: u32) -> usize {
     let dos_header = module_base as PIMAGE_DOS_HEADER;
 
@@ -192,6 +197,7 @@ unsafe fn rva_to_file_offset_pointer(module_base: usize, mut rva: u32) -> usize 
 
     return 0;
 }
+*/
 
 //https://github.com/Ben-Lichtman/reloader/blob/7d4e82b64f0ee6bf56dec47153721f62e207faa7/src/helpers.rs#L18
 pub const fn fnv1a_hash_32_wstr(wchars: &[u16]) -> u32 {
